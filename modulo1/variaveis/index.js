5dd7762309d7c0c12bba90798835f89c7c508cbd
Roberto Alves Coelho <roberto.alvescoelho@gmail.com>
2021-11-09 12:42:35 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,51 @@
+/* 
+// 1
+10
+10, 5
+// 2
+10, 10, 10
+// 3
+horaTrabalhada
+valorDiaria
+// 1
+let nomeUser = ""
+let idadeUser = ""
+typeof nome
+typeof idade
+//a variavel não foi definida
+ 
+const nomeUser = prompt('Qual é o seu nome completo?')
+ const idadeUser = prompt('Qual é a sua idade?')
+
+ console.log("Variável nomeUser:", typeof nomeUser)
+ console.log("Variável idadeUser:", typeof idadeUser)
+//as duas retornam strings
+console.log('Olá', nomeUser, 'você tem', idadeUser, 'anos.')
+// 2
+const corRoupa = prompt('Você está usando uma roupa vermelha?')
+console.log(corRoupa)
+const cafe = prompt('Você gosta de café?')
+console.log(cafe)
+const esporte = prompt('Você pratica esporte?')
+console.log(esporte)
+// 3
+let a = 10
+let b = 25
+let c = a
+a =  b
+b = c
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+*/
+
+const numeroUm = prompt('Digite um número!')
+const numeroDois = prompt('Digite outro número!')
+
+console.log(parseInt(numeroUm) + parseInt(numeroDois))
+console.log(numeroUm * numeroDois)
+
+
+
+
+
