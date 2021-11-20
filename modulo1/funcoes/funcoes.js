a1b2e765ab95d84ea4416be6c85d69681e2c598f
Roberto Alves Coelho <roberto.alvescoelho@gmail.com>
2021-11-19 23:23:39 -0300

    funcoes

---
+++ modulo1/funcoes/funcoes.js
@@ -0,0 +1,107 @@
+/*
+10
+50
+
+não aparece nada no console
+
+se e verdadeiro ou falso o que o usuario esta digitando
+
+true
+true
+true
+*/
+/*
+function apresentaçao(){
+console.log('Eu sou Roberto, tenho 33 anos, moro em Saquarema e sou estudante.')
+}
+apresentaçao()
+*/
+/*
+function apresentaçao({nome, idade, cidade, profissao}){
+    console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade}, e sou ${profissao}`)   
+}
+apresentaçao({nome: 'Roberto', idade: 33, cidade: 'saquarema', profissao: 'Estudante'});
+*/
+/*
+function funcao(x, y){
+    const resultado = x + y;
+    return resultado;
+}
+console.log(funcao(2, 3))
+*/
+/*
+function funcao(x, y){
+    const resultado = x >= y;
+    return resultado;
+}
+console.log(funcao(2, 3))
+*/
+/*
+function funcao(x){
+    if (x % 2 == 0) 
+    {
+    console.log('Par') 
+        }
+    else{     
+    console.log('ímpar')
+        
+        }
+}
+console.log(funcao(6))
+*/
+/*
+function caracters(x) {   
+console.log(x.length)
+console.log(x.toUpperCase())
+return caracters
+}
+caracters('Robertinho')
+*/
+/*
+const num1 = Number(prompt('Digite um numero'))
+const num2 = Number(prompt('Digite outro numero'))
+function soma(x , y){
+const resultado = x + y;
+return resultado
+
+}
+const resultado = soma(num1, num2)
+console.log(resultado)
+*/
+/*
+const num1 = Number(prompt('Digite um numero'))
+const num2 = Number(prompt('Digite outro numero'))
+function multiplicacao(x , y){
+const resultado = x * y;
+return resultado
+
+}
+const resultado = multiplicacao(num1, num2)
+console.log(resultado)
+*/
+/*
+const num1 = Number(prompt('Digite um numero'))
+const num2 = Number(prompt('Digite outro numero'))
+function subtracao(x , y){
+const resultado = x - y;
+return resultado
+
+}
+const resultado = subtracao(num1, num2)
+console.log(resultado)
+*/
+
+
+const num1 = Number(prompt('Digite um numero'))
+const num2 = Number(prompt('Digite outro numero'))
+function divizao(x , y){
+const resultado = x / y;
+return resultado
+
+}
+const resultado = divizao(num1, num2)
+console.log(resultado)
+
+
+
+
